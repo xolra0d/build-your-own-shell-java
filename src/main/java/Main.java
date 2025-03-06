8e9739934c80ad8fb308c8c9f610685fc282b31f
lolka0d <fockork@gmail.com>
2025-03-06 12:54:46 +0200

    2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,5 +6,7 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        System.out.println(input + ": command not found");
+
     }
 }
