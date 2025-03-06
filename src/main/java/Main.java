b0a3aae101f3920a8484dac3102dc4d73423048e
lolka0d <fockork@gmail.com>
2025-03-06 12:56:33 +0200

    3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
+        while (true) {
+            System.out.print("$ ");
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input + ": command not found");
-
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            System.out.println(input + ": command not found");
+        }
     }
 }
